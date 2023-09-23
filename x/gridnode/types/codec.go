8df264bca0464651348d6cbf90426e5ef7f2816d
dekm <gevan73@gmail.com>
2023-09-23 13:00:45 +0200

    add back MsgGridnodeDelegate and MsgGridnodeDelegate

--- x/gridnode/types/codec.go
+++ x/gridnode/types/codec.go
@@ -3,6 +3,7 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 	// this line is used by starport scaffolding # 1
 )
@@ -12,18 +13,12 @@ func RegisterCodec(cdc *codec.LegacyAmino) {
 }
 
 // RegisterInterfaces registers the interfaces types with the interface registry.
-// func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-// 	registry.RegisterImplementations(
-// 		(*sdk.Msg)(nil),
-// 		&MsgGridnodeDelegate{},
-// 		&MsgGridnodeUndelegate{},
-// 	)
-
-// 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
-// }
-
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
-	// this line is used by starport scaffolding # 3
+	registry.RegisterImplementations(
+		(*sdk.Msg)(nil),
+		&MsgGridnodeDelegate{},
+		&MsgGridnodeUndelegate{},
+	)
 
 	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
