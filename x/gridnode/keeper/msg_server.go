5936dfa8edcf033621ab861f630e88630cc92d88
dekm <gevan73@gmail.com>
2023-09-26 14:29:56 +0200

    modify the msg_server

--- x/gridnode/keeper/msg_server.go
+++ x/gridnode/keeper/msg_server.go
@@ -1,6 +1,9 @@
 package keeper
 
 import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/unigrid-project/cosmos-sdk-gridnode/x/gridnode/types"
 )
 
@@ -15,3 +18,26 @@ func NewMsgServerImpl(keeper *Keeper) types.MsgServer {
 }
 
 var _ types.MsgServer = msgServer{}
+
+func (s *msgServer) DelegateTokens(ctx context.Context, req *types.MsgGridnodeDelegate) (*types.MsgGridnodeDelegateResponse, error) {
+	// Convert context.Context to sdk.Context
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+	// Extract information from the request
+	delegatorAddr, err := sdk.AccAddressFromBech32(req.DelegatorAddress)
+	if err != nil {
+		return nil, err
+	}
+	amount := sdk.NewInt(req.Amount)
+
+	// Call the Keeper's DelegateTokens method to perform the business logic
+	err = s.Keeper.DelegateTokens(sdkCtx, delegatorAddr, amount)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct and return a response
+	return &types.MsgGridnodeDelegateResponse{
+		// Populate with any needed response data
+	}, nil
+}
